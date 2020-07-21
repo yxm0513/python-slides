83e3b31162f2b97582af7494df9f5038753aa57e
yxm0513 <simon.yang.sh@gmail.com>
2020-07-21 21:58:59 +0800

    bug fix

---
+++ src/args.py
@@ -0,0 +1,14 @@
+def foo(*args, **kwargs):
+    print('args = ', args)
+    print('kwargs = ', kwargs)
+    print('---------------------------------------')
+
+if __name__ == '__main__':
+    foo(1,2,3,4)
+    foo(a=1,b=2,c=3)
+    foo(1,2,3,4, a=1,b=2,c=3)
+    foo('a', 1, None, a=1, b='2', c=3)
+    a = (1,2,3)
+    b = {'1':2, '3':4}
+    foo(a, b)
+    foo(*a, **b)
